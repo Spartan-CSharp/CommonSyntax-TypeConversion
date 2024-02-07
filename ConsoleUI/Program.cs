9ba31dbba08c4f3acbeefc205476ec3efa9700af
Pierre Plourde <pierre@spartancsharp.net>
2024-02-07 12:13:20 -0500

    Create Code
    Created a Console Application that captures a user's age from the Console and then identifies how old they were in the year 2000. If they were not born yet, it tells them that instead.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,30 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
+			Console.Write("How old are you: ");
+			string agetext = Console.ReadLine();
+
+			bool isagevalid = int.TryParse(agetext, out int age);
+
+			if ( isagevalid )
+			{
+				age -= 21;
+
+				if ( age < 0 )
+				{
+					Console.WriteLine("You were not born yet in the year 2000!");
+				}
+				else
+				{
+					Console.WriteLine($"You were {age} years old in the year 2000.");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Please enter your age using digits.");
+			}
+
+			_ = Console.ReadLine();
 		}
 	}
 }
